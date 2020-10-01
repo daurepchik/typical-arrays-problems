26ba5ddd9a4ffd9783757739ef9d81c491737667
Aidenov Dauren <daurenblin5@gmail.com>
2020-10-01 16:11:18 +0500

    feat: implement solituion for task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array) return 0;
+    if (array.length === 0) return 0;
+
+    let min = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) min = array[i]
+    }
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array) return 0;
+    if (array.length === 0) return 0;
+
+    let max = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > max) max = array[i]
+    }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array) return 0;
+    if (array.length === 0) return 0;
+
+    let avg = 0;
+    let total = 0;
+    for (let i = 0; i < array.length; i++) {
+        total += array[i]
+    }
+    avg = total / array.length;
+    return avg;
 }
